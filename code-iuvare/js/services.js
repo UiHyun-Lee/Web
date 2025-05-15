bb6b3fd49db962da41bce9c13aa000695b825ef5
uihyunlee <deutschluh@gmail.com>
2025-05-15 22:46:33 +0200

    clients want another concept

--- code-iuvare/js/services.js
+++ code-iuvare/js/services.js
@@ -4,13 +4,19 @@ function toggleAccordion(headerEl) {
 
     const isOpen = contentEl.style.height && contentEl.style.height !== "0px";
 
+
     if (isOpen) {
         contentEl.style.height = "0px";
         contentEl.style.opacity = "0";
-        icon.textContent = "+";
+
     } else {
         contentEl.style.height = contentEl.scrollHeight + "px";
         contentEl.style.opacity = "1";
-        icon.textContent = "−";
     }
+}
+function rotateImage() {
+    currentRotation += 90;
+
+    const image = document.querySelector('.toggle-icon');
+    image.style.transform = `rotate(${currentRotation % 360}deg)`;
 }
